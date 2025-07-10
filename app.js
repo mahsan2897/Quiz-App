2bbf5078ffe21e41dabfc77ab65d180dd885db8c
Muhammad Ahsan <muhammadahsan2897@gmail.com>
2025-07-10 10:36:00 +0500

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const nextBtn = document.getElementById('nextBtn');
     const questionBox = document.getElementById('questionBox');
     const quizContainer = document.getElementById('quizContainer');
+    const timerBox = document.getElementById('timerBox')
 
     let currentQuestion = 0;
     let userAnswer = '';
     let score = 0;
     let questions = [];
+    let timer;
+    let timeLeft = 60;
+    
 
     // Load questions from API
     fetch('https://the-trivia-api.com/v2/questions')
@@ -20,22 +24,65 @@ const nextBtn = document.getElementById('nextBtn');
 
     // Show current question
     function showQuestion() {
-      const q = questions[currentQuestion];
-      const allAnswers = [...q.incorrectAnswers];
-      const correctIndex = Math.floor(Math.random() * (allAnswers.length + 1));
-      allAnswers.splice(correctIndex, 0, q.correctAnswer);
+  clearInterval(timer); // Clear any existing timer
+  timeLeft = 30;
+  startTimer();
 
-      // Display question
-      questionBox.innerHTML = `<p class="fw-bold">${q.question.text}</p>`;
-      allAnswers.forEach((answer, index) => {
-        questionBox.innerHTML += `
-          <div>
-            <input type="radio" name="answer" value="${answer}" id="ans${index}" onchange="selectAnswer('${answer}')">
-            <label for="ans${index}">${answer}</label>
-          </div>
-        `;
-      });
+  const q = questions[currentQuestion];
+  const allAnswers = [...q.incorrectAnswers];
+  const correctIndex = Math.floor(Math.random() * (allAnswers.length + 1));
+  allAnswers.splice(correctIndex, 0, q.correctAnswer);
+
+  questionBox.innerHTML = `<p class="fw-bold">${q.question.text}</p>`;
+  allAnswers.forEach((answer, index) => {
+    questionBox.innerHTML += `
+      <div>
+        <input type="radio" name="answer" value="${answer}" id="ans${index}" onchange="selectAnswer('${answer}')">
+        <label for="ans${index}">${answer}</label>
+      </div>
+    `;
+  });
+
+  nextBtn.disabled = true; // Disable button initially
+}
+
+function moveToNextQuestion() {
+  if (userAnswer === questions[currentQuestion].correctAnswer) {
+    score++;
+  }
+
+  currentQuestion++;
+  userAnswer = '';
+  nextBtn.disabled = true;
+
+  if (currentQuestion < 10) {
+    showQuestion();
+  } else {
+    showResult();
+  }
+}
+
+function startTimer() {
+  timerBox.innerText = `Time left: ${timeLeft}s`;
+
+  timer = setInterval(() => {
+    timeLeft--;
+    timerBox.innerText = `Time left: ${timeLeft}s`;
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      moveToNextQuestion(); // 
     }
+  }, 300);
+    if (timeLeft <= 0) {
+  clearInterval(timer);
+  alert("Time's up!");
+  moveToNextQuestion();
+}
+}
+
+
+
 
     // Called when user selects an answer
     function selectAnswer(answer) {
@@ -45,26 +92,30 @@ const nextBtn = document.getElementById('nextBtn');
 
     // Go to next question
     nextBtn.addEventListener('click', () => {
-      if (userAnswer === questions[currentQuestion].correctAnswer) {
-        score++;
-      }
+  clearInterval(timer);
+  moveToNextQuestion();
 
-      currentQuestion++;
-      userAnswer = '';
-      nextBtn.disabled = true;
+  if (userAnswer === questions[currentQuestion].correctAnswer) {
+    score++;
+  }
 
-      if (currentQuestion < 10) {
-        showQuestion();
-      } else {
-        showResult();
-      }
-    });
+  currentQuestion++;
+  userAnswer = '';
+  nextBtn.disabled = true;
+
+  if (currentQuestion < 10) {
+    showQuestion();
+  } else {
+    showResult();
+  }
+});
+
 
     // Show final score
     function showResult() {
       const gifUrl = score > 7
       quizContainer.innerHTML = `
-        <div class="card mx-auto border-0" style="width: 18rem;">
+        <div class="card mx-auto bg-info w-100" >
           <div class="card-body text-center">
             <h5 class="card-title">Your Score</h5>
             <p class="card-text">${score} out of 10</p>
